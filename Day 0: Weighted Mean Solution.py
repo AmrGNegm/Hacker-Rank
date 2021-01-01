a174bc5528c5ee3795d7f60b01ecad9110c91ac2
Amr Negm <amr.g.negm@gmail.com>
2021-01-02 00:38:27 +0200

    Day 0: Weighted Mean Solution.py

---
+++ Day 0: Weighted Mean Solution.py
@@ -0,0 +1,8 @@
+n = int(input())
+x = list(map(int, input().split()))
+w = list(map(int, input().split()))
+dividend, divisor = 0.0, 0
+for i in range(n):
+    dividend += x[i] * w[i]
+    divisor += w[i]
+print ("%0.1f" % (dividend/divisor))
